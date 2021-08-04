681d266ffe1a6ca089e8549576c2c5b416458edd
bocman17 <hejlikt@gmail.com>
2021-08-04 02:33:27 +0200

    changeBGcolor

---
+++ script.js
@@ -0,0 +1,12 @@
+let myForm = document.getElementById("my-form")
+let myBody = document.querySelector("body")
+
+myForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    let inputColor = e.target.elements.color.value
+
+    myBody.style.backgroundColor = inputColor
+
+    e.target.elements.color.value = ""
+})
